7c4c7d315db646053734c8aa0bcbc05801518ea5
ygygy2022 <yu.guo4@griffithuni.edu.au>
2024-08-10 20:48:25 +1000

    finish all form function
    add validation from server side

--- lib/action-server.ts
+++ lib/action-server.ts
@@ -2,8 +2,75 @@
 
 import { MealO } from "@/app/meals/models/MealData";
 import { saveMeal } from "./meals";
-
-export default async function shareMeal(formData: FormData) {
+import { redirect } from "next/navigation";
+import { promises } from "dns";
+export default async function shareMeal(prevState: any, formData: FormData) {
   const meal = new MealO(formData);
+  const errors = validateMealObject(meal);
+  if (errors.length > 0) {
+    return { message: errors };
+  }
   await saveMeal(meal);
+  redirect("/meals");
+}
+
+function validateMealObject(meal: MealO): string[] {
+  const errors = [];
+
+  //inspect  title
+  if (
+    !meal.title ||
+    typeof meal.title !== "string" ||
+    meal.title.trim().length === 0
+  ) {
+    errors.push("Invalid title: must be a non-empty string.");
+  }
+
+  // inspect summary
+  if (
+    !meal.summary ||
+    typeof meal.summary !== "string" ||
+    meal.summary.trim().length === 0
+  ) {
+    errors.push("Invalid summary: must be a non-empty string.");
+  }
+
+  // inspect instructions
+  if (
+    !meal.instructions ||
+    typeof meal.instructions !== "string" ||
+    meal.instructions.trim().length === 0
+  ) {
+    errors.push("Invalid instructions: must be a non-empty string.");
+  }
+
+  // inspect creator
+  if (
+    !meal.creator ||
+    typeof meal.creator !== "string" ||
+    meal.creator.trim().length === 0
+  ) {
+    errors.push("Invalid creator: must be a non-empty string.");
+  }
+
+  // inspect creator_email
+  if (
+    !meal.creator_email ||
+    typeof meal.creator_email !== "string" ||
+    !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(meal.creator_email)
+  ) {
+    errors.push("Invalid creator_email: must be a valid email address.");
+  }
+
+  // inspect image
+  if (
+    !meal.image ||
+    typeof meal.image !== "string" ||
+    (meal.image as File).size
+  ) {
+    errors.push("Invalid image: must be a valid file path.");
+  }
+
+  // if there are no errors, return null
+  return errors;
 }
